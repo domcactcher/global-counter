064cc31768812452ef86e57c2a33fb4f5a38bd3b
nanummer <39667378+nanummer@users.noreply.github.com>
2019-07-20 02:56:15 +0900

    no need to stringfy value to save in local storage

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
      document.getElementById('display').innerHTML = parseInt(localStorage.getItem("counter"));
  }
  function increaseCounter() {
-     var a = parseInt(localStorage.getItem('counter')) + 1;
-     var b = a.toString();
-     localStorage.setItem('counter', a.toString());
+   /* 
+    * not necessary to stringfy the value to save in local storage. 
+    *
+    *  var a = parseInt(localStorage.getItem('counter')) + 1;
+    *  var b = a.toString();
+    *  localStorage.setItem('counter', a.toString());
+    */
+    var a = parseInt(localStorage.getItem('counter')) + 1;
+    // localStorage value is string.
+    localStorage.setItem('counter', a);
     document.getElementById('display').innerHTML = parseInt(localStorage.getItem("counter"));
  }
