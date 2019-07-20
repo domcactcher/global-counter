1ecb40024d14c0889145c187b9470cc61b7dfaf7
nanummer <39667378+nanummer@users.noreply.github.com>
2019-07-20 14:02:02 +0900

    deleted window var

--- index.js
+++ index.js
@@ -3,15 +3,7 @@
      document.getElementById('display').innerHTML = parseInt(localStorage.getItem("counter"));
  }
  function increaseCounter() {
-   /* 
-    * not necessary to stringfy the value to save in local storage. 
-    *
-    *  var a = parseInt(localStorage.getItem('counter')) + 1;
-    *  var b = a.toString();
-    *  localStorage.setItem('counter', a.toString());
-    */
     var a = parseInt(localStorage.getItem('counter')) + 1;
-    // localStorage value is string.
     localStorage.setItem('counter', a);
     document.getElementById('display').innerHTML = parseInt(localStorage.getItem("counter"));
  }
